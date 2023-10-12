944fe52a3f5b47d85fc467eea67b5b0e4014249f
Xuanfq <2624208682@qq.com>
2023-10-13 00:37:51 +0800

    Program entry to view version information

--- autorunx.py
+++ autorunx.py
@@ -1,42 +1,41 @@
 
-import sys,os,argparse
+import sys
+import os
+import argparse
 import globals
 import json
 
 
-parser=argparse.ArgumentParser()
-parser.add_argument('-c','--config', help="specifies the configuration file to run", required=False,type=str)
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--config', required=False, type=str,
+                    help="specifies the configuration file to run")
+parser.add_argument('-v', '--version', action='version', version='1.0.1',
+                    help="checking version information"
+                    )
 
 
-
-def main(config_path=None):
-    args=parser.parse_args()
+def main():
+    args = parser.parse_args()
     if args.config is not None:
-        config_path=args.config
-    elif config_path is not None:
-        pass
-    else:
-        print("usage: [-h] -c CONFIG\n: error: the following arguments are required: -c/--config")
+        config_path = args.config
+        if not os.path.exists(config_path):
+            print("Invalid parameter: {}".format(config_path))
+            return
+        with open(config_path, "r", encoding="utf-8") as f:
+            config = json.load(f)
+            globals.init(**config)
+            globals.start()
         return
-    with open(config_path,"r",encoding="utf-8") as f:
-        config=json.load(f)
-        globals.init(**config)
-        globals.start()
+    else:
+        parser.print_help()
 
 
 def run(config):
-    with open(config,"r",encoding="utf-8") as f:
-        config=json.load(f)
+    with open(config, "r", encoding="utf-8") as f:
+        config = json.load(f)
         globals.init(**config)
         globals.start()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
